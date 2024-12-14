0f2d11267cd6ffa9151aa1d1b9ce1891d9d4cd5b
Olive Njoroge <njorogeolive@gmail.com>
2024-12-14 08:59:02 +0300

    Added score recording functionality

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.querySelector("#playerDisplay");
 const computerDisplay = document.querySelector("#computerDisplay");
-const resultrDisplay = document.querySelector("#resultDisplay");
+const resultDisplay = document.querySelector("#resultDisplay");
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random()*3)];
@@ -29,5 +33,22 @@ function playGame(playerChoice){
     playerDisplay.innerHTML = `PLAYER: ${playerChoice}`;
     computerDisplay.innerHTML = `COMPUTER: ${computerChoice}`;
     resultDisplay.innerHTML = result;
+
+    resultDisplay.classList.remove("greentext", "redtext");
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greentext");
+            playerScore ++;
+            playerScoreDisplay.innerHTML = playerScore;
+            break;
+
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redtext");
+            computerScore ++;
+            computerScoreDisplay.innerHTML = computerScore;
+
+            break;
+    }
 
 }
